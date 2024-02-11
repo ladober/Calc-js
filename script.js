fae1c24e50527749492836dd2c72c26db794e247
ladober <lado.beridze@gmail.com>
2024-02-11 20:08:28 +0400

    work done

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const percent = document.querySelector(".percent");
 
 const del = document.querySelector(".delete");
 
+const x2 = document.querySelector(".x2");
+
 for (let i = 0; i < calcBtnList.length; i++) {
   calcBtnList[i].addEventListener("click", () => {
     calcSum.textContent += calcBtnList[i].textContent;
@@ -32,4 +34,9 @@ del.addEventListener("click", () => {
   let newCalSum = calcSum.textContent;
   newCalSum = newCalSum.substring(0, newCalSum.length - 1);
   calcSum.textContent = newCalSum;
+});
+
+x2.addEventListener("click", () => {
+  let result = eval(calcSum.textContent * calcSum.textContent);
+  calcSum.textContent = result;
 });
